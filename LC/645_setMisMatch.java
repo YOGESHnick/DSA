799989fc1277c822e739f05843205669e315ab40
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-08-10 10:32:16 +0530

    Create 645_setMisMatch.java

---
+++ LC/645_setMisMatch.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=index+1){
+                return new int[] {arr[index],index+1};
+            }
+        }
+        return new int[] {-1,-1};
+    }
+    void swap(int[] arr,int f,int s){
+        int t=arr[f];
+        arr[f]=arr[s];
+        arr[s]=t;
+    }
+}
