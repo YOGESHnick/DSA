52cee95c338dce73cc9860bb7348c372dbb6b98e
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-08-10 12:23:41 +0530

    Create 1.1while loop.java

---
+++ 1.1while loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int e=0;
+		int o=0;
+		while(n>0){
+			int x = n%10;
+			if(x%2==0){e=e+x;}
+			else{o=o+x;}
+			n=n/10;
+		}
+		System.out.print(e+" "+o);
+	}
+}
