05d83f97419ce3f41f736ad0003d9f2ddd146de9
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-09-19 21:27:52 +0530

    Create 3.1-leftRotateOnce.java

---
+++ 3.1-leftRotateOnce.java
@@ -0,0 +1,12 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    static int[] rotateArray(int[] arr, int n) {
+        int temp = arr[0];
+        for(int i=0;i<n-1;i++){
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=temp;
+        return arr;
+    }
+}
