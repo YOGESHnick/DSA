ba1e277c0e7ecde828cb0768415a2438dd5bec32
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-09-22 22:47:51 +0530

    Update lowerBound.java

--- lowerBound.java
+++ lowerBound.java
@@ -1,15 +1,12 @@
-
 public class Solution {
     public static int lowerBound(int []arr, int n, int x) {
         int low=0;
         int high=n-1;
-        int ans=n;
         while(low<=high)
         {
             int mid=(low+high)/2;
             if(arr[mid]>=x)
             {
-                ans=mid;
                 high=mid-1;
             }
             else
@@ -17,6 +14,6 @@ public class Solution {
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
 }
