e28cf0a59e46e5a44c96bde3edad77caecdcf340
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-09-22 22:40:48 +0530

    Create 2149. Rearrange Array Elements by Sign.java

---
+++ LC/2149. Rearrange Array Elements by Sign.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] a) {
+       int b[]=new int[a.length];
+       int j=0;
+       int k=0;
+       for(int i=0;i<a.length;i++){
+            if(a[i]>0){
+                b[j]=a[i];
+                j=j+2;
+            }
+            else if(a[i]<0){
+                b[k+1]=a[i];
+                k=k+2;
+            }
+        }
+        return b;
+    }
+}
