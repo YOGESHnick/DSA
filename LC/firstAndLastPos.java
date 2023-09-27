bca8d2347524ec592d12f721e3c574644205d1af
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-09-27 20:03:11 +0530

    Create firstAndLastPos.java

---
+++ LC/firstAndLastPos.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        int ans[]=new int[2];
+        ans[0]=searchFirst(nums,target);
+        ans[1]=searchLast(nums,target);
+        return ans;
+    }
+    public int searchFirst(int[] nums,int target){
+        int low=0;
+        int res=-1;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+
+            if(nums[mid]==target){
+                res=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return res;
+    }
+    public int searchLast(int[] nums,int target){
+        int low=0;
+        int res=-1;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+
+            if(nums[mid]==target){
+                res=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return res;
+    }
+}
