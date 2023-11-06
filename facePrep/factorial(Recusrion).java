0776ab174f134cbd54ef2702407329f91ea715e2
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2023-11-06 08:34:26 +0530

    Create factorial(Recusrion).java

---
+++ facePrep/factorial(Recusrion).java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int fact = n*n-1;
+    System.out.print("The factorial of "+n+ " is " + fact(n));
+  }
+  public static int fact(int n)
+  {
+    if(n==1)
+      return 1;
+    else{
+      return  n*fact(n-1);
+    }
+  }
+}
