8fae75f868576a83c4ffb1893205adee7b933e1a
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-03-17 08:00:00 +0530

    Update 46. Permutations.java

--- LC/46. Permutations.java
+++ LC/46. Permutations.java
@@ -24,3 +24,33 @@ class Solution {
         return answer;
     }
 }
+//  approach 2 --> reduces space complexity
+
+class Solution {
+    public void possibilities(int[] nums, List<List<Integer>> answer, int start) {
+        if (start == nums.length - 1) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            answer.add(permutation);
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            possibilities(nums, answer, start + 1);
+            swap(nums, start, i);
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        possibilities(nums, answer, 0);
+        return answer;
+    }
+}
