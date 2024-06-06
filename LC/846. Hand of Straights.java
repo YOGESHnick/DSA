0f4e70d4d5cd9e835c85b74650a7f532b2877d7c
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-06-06 08:43:42 +0530

    Create 846. Hand of Straights.java

---
+++ LC/846. Hand of Straights.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int card : hand) {
+            count.put(card, count.getOrDefault(card, 0) + 1);
+        }
+
+        int[] sortedKeys = new int[count.size()];
+        int index = 0;
+        for (int key : count.keySet()) {
+            sortedKeys[index++] = key;
+        }
+        Arrays.sort(sortedKeys);
+        
+        for (int key : sortedKeys) {
+            if (count.get(key) > 0) { 
+                int startCount = count.get(key);
+
+                for (int i = key; i < key + groupSize; i++) {
+                    if (count.getOrDefault(i, 0) < startCount) {
+                        return false;
+                    }
+                    count.put(i, count.get(i) - startCount);
+                }
+            }
+        }
+        return true;
+    }
+}
