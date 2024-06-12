f5134e2214479e230282fd9fe90eca1962d877fd
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-06-12 07:08:19 +0530

    Update 75.SortColors.java, use Dutch National Flag algo

--- LC/75.SortColors.java
+++ LC/75.SortColors.java
@@ -1,15 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if ( nums[i] == 0) cnt0++;
-            else if (nums[i] == 1) cnt1++;
-            else cnt2++;
+        int low=0, mid=0, high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
         }
-
-        for (int i = 0; i < cnt0; i++) nums[i]=0; // replacing 0's
-        for (int i = cnt0; i < cnt0 + cnt1; i++) nums[i]=1; // replacing 1's
-        for (int i = cnt0 + cnt1; i < n; i++) nums[i]=2;
     }
 }
