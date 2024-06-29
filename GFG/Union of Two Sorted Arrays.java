d13e2f211fd26a11b4db2849cdca9d4045e9d3a7
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-06-29 09:23:50 +0530

    Create Union of Two Sorted Arrays.java

---
+++ GFG/Union of Two Sorted Arrays.java
@@ -0,0 +1,40 @@
+//https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays
+ArrayList<Integer> union = new ArrayList<>();
+        
+        int i = 0, j = 0;
+        
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
+                    union.add(arr2[j]);
+                }
+                j++;
+            } else {
+                if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        
+        while (i < n) {
+            if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                union.add(arr1[i]);
+            }
+            i++;
+        }
+        
+        while (j < m) {
+            if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
+                union.add(arr2[j]);
+            }
+            j++;
+        }
+        
+        return union;
