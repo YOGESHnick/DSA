9fda32ccd5d00c17763cd0d2a520be00646b9de2
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-07-14 19:47:54 +0530

    Update kadane's Algo.java

--- GFG/kadane's Algo.java
+++ GFG/kadane's Algo.java
@@ -1,14 +1,10 @@
-class Solution{
+class Solution {
 
-    long maxSubarraySum(int arr[], int n){
+    long maxSubarraySum(int[] arr) {
         long sum=arr[0],max=arr[0];
         for(int i=1;i<arr.length;i++){
-            if(sum>=0){
-                sum+=arr[i];
-            }
-            else{
-                sum=arr[i];
-            }
+            if(sum>0) sum+=arr[i];
+            else sum=arr[i];
             max=Math.max(sum,max);
         }
         return max;
