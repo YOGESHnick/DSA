21a09865b7eb673cd715987e67f56942b80eab3b
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-12-03 10:59:30 +0530

    Create Majority Element II.java

---
+++ GFG/160DaysChallenge/Majority Element II.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        // 1. store frequency of each num in hashmap
+        // 2. iterate, if frequency > length/3 add to list
+        // 3. return list
+        
+        // store frequency of each num in hashmap
+        Map<Integer, Integer> numberFrequency = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numberFrequency.put(nums[i], numberFrequency.getOrDefault(nums[i], 0) + 1);
+        }
+        // iterate, if frequency > length/3 add to list
+        List<Integer> majorityElements = new ArrayList<>();
+        int threshold = nums.length / 3;
+        
+        for (Map.Entry<Integer, Integer> entry : numberFrequency.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+            if (count > threshold) {
+                majorityElements.add(element);
+            }
+        }
+        return majorityElements;
+    }
+}
