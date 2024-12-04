0016e29f3feb8ddb7444dc66916cac1547efa2ad
YOGESH SHAMLIN SHINANTH <108965130+YOGESHnick@users.noreply.github.com>
2024-12-04 10:38:12 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed.java

---
+++ GFG/160DaysChallenge/Stock Buy and Sell – Multiple Transaction Allowed.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int profit = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return profit;
+    }
+}
